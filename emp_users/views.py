da0658cd2ea1fbf8e540a4ba3df62b1d8db66eb6
CHaKU <kuover@gmail.com>
2025-01-20 09:51:03 +0700

    httprequest

--- emp_users/views.py
+++ emp_users/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     return render(request,"emp_users/index.html")
